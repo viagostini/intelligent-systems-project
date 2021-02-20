870f0423266d04af3cac2b9c3cca8cc96d878ff9
Vinicius Agostini <vinicius.agostini19@gmail.com>
2021-02-20 10:02:10 -0300

    :sparkles: enviroment class to manage env variables

--- server/models.py
+++ server/models.py
@@ -1,7 +1,17 @@
+import os
 from typing import List
 
 import pandas as pd
-from pydantic import BaseModel
+from dotenv import load_dotenv
+from pydantic import BaseModel, BaseSettings
+
+if os.environ.get("COMPOSE_PROJECT_NAME") is None:
+    load_dotenv(".env.local")
+
+
+class Environment(BaseSettings):
+    model_path: str
+    test_products_path: str
 
 
 class Product(BaseModel):

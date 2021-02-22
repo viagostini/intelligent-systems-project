8c975decdeeb0508f015741b6438d663f31c050e
Vinicius Agostini <vinicius.agostini19@gmail.com>
2021-02-22 18:42:49 -0300

    :sparkles: locust user behaves more randomly

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,26 @@
 import json
+from random import sample
 
 from locust import HttpUser, between, task
+from numpy.random import normal
 
 API_ROOT = "http://localhost:5000"
 
 
-class Getter(HttpUser):
-    """This user performs predictions on the same payload"""
+class User(HttpUser):
+    """
+    This user performs predictions on a randomized payload with the number of elements
+    in the request following a normal distribution with mean 100 and standard deviation
+    of 20. The data itself is a single product repeated many times.
+    """
 
     host = API_ROOT
-    shortlink = ""
     wait_time = between(0.005, 0.01)
 
     def on_start(self):
         with open("data/test_products.json", "r") as json_file:
-            self.data = json.load(json_file)
+            raw_data = json.load(json_file)["products"]
+            self.data = {"products": sample(raw_data, 1) * int(normal(100, 20))}
 
     @task
     def get_url(self):
